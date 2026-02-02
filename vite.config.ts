8129a5309756823925943a0c33e63f204050561a
27180781 <37129224+27180781@users.noreply.github.com>
2026-02-03 00:48:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: "./", // For Electron
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    // הסרנו את componentTagger כדי לאפשר בנייה ל-Electron
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
